00bcb3e3dc26dcb4fd89cef9444f046a6a335778
Lucy Hutcheson <hutcheson.lucy@gmail.com>
2012-05-03 13:03:42 -0500

    Reorganized code for better readability.

--- ASD/js/main.js
+++ ASD/js/main.js
@@ -3,58 +3,99 @@
  * Created for:  Advanced Scalable Data Infrastructures 1205
  */
 
-$(document).ready(function(){
-	
+$(document).ready(function() {
+
 
 	//Variable defaults
 	var bibleTopics = ["--Choose A Topic--", "Christian Life", "Marriage", "Family"],
 		audienceValue;
-	makeTopics();
-	setDate();
 	var category = getUrlVars()["cat"];
 	var lessonId = getUrlVars()["lessonId"];
 	var op = getUrlVars()["op"];
 
-	bible_book_disclosure();
-	$("#focus").change(function(){
-		bible_book_disclosure(); 
-	});
 
+	// Get value from URL
+	function getUrlVars() {
+		var vars = {};
+		var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+			vars[key] = value;
+		});
+		return vars;
+	}
 
 	// Responsive Disclosure
-	function bible_book_disclosure(){
-		$('#ot-books').hide();
-		$('#nt-books').hide();
-		if($("#focus").val() == "Old Testament") {
+	function bible_book_disclosure() {
+	$('#ot-books').hide();
+	$('#nt-books').hide();
+		if ($("#focus").val() === "Old Testament") {
 			$("#ot-books").show();
-		}else if($("#focus").val() == "New Testament"){
+		} else if ($("#focus").val() === "New Testament") {
 			$("#nt-books").show();
 		}
+
 	}
+	$('#ot-books').hide();
+	$('#nt-books').hide();
+	$("#focus").change(function(e){
+		e.preventDefault();
+		bible_book_disclosure();
+	});
 	
+	
+	// Pull the right JSON Data
+	switch (category) {
+		case "Adults":
+			var audience = "Adults";
+			getData(json.adults.lessons);
+		 	break;
+		case "Men":
+			var audience = "Men";
+			getData(json.men.lessons);
+		  	break;
+		case "Women":
+			var audience = "Women";
+			getData(json.women.lessons);
+		  	break;
+		case "Youth":
+			var audience = "Youth";
+			getData(json.youth.lessons);
+		  	break;
+		case "Children":
+			var audience = "Children";
+			getData(json.children.lessons);
+		  	break;
+	}
+
+	//Add default data if there is none in local storage
+	function autoFillData(audience){
+		//Store the JSON object in local storage
+		for(var n in audience){
+			var id = Math.floor(Math.random()*10000001);
+			localStorage.setItem(id, JSON.stringify(audience[n]));
+		}
+	} 
 
 
+	// Get Lessons and Build list
 	function getData(audience) {
 		// Update page header title
 		var category = getUrlVars()["cat"];
-		$('h1#headerTitle').replaceWith('<h2>'+ category + '</h2>');
+		$('h1#headerTitle').replaceWith('<h2>' + category + '</h2>');
 
 		$('#errors').empty(); //Reset error messages
 
 		if (localStorage.length === 0) {
 			var ask = confirm("There are no lessons in local storage.  Do you want to load default lessons?");
-			if(ask){
+			if (ask) {
 				autoFillData(audience);
-			}else{
+			} else {
 				alert("Lessons were not loaded.");
 				return false;
 			}
 		}
 
-
 		// Create list items from sorted storage array
 		for (var i=0, len=localStorage.length; i<len; i++){
-		
 			var key = localStorage.key(i);
 			var value = localStorage.getItem(key);
 			// convert the string back to an object
@@ -92,30 +133,21 @@ $(document).ready(function(){
 
 	}
 
-	switch (category) {
-		case "Adults":
-			var audience = "Adults";
-			getData(json.adults.lessons);
-		 	break;
-		case "Men":
-			var audience = "Men";
-			getData(json.men.lessons);
-		  	break;
-		case "Women":
-			var audience = "Women";
-			getData(json.women.lessons);
-		  	break;
-		case "Youth":
-			var audience = "Youth";
-			getData(json.youth.lessons);
-		  	break;
-		case "Children":
-			var audience = "Children";
-			getData(json.children.lessons);
-		  	break;
-	}
+	// Sort my lesson list after it has been created
+	var mylist = $('#lesson-list');
+	var listitems = mylist.children('li').get();
+	listitems.sort(function(a, b) {
+	var compA = $(a).text().toUpperCase();
+	var compB = $(b).text().toUpperCase();
+	// Currently set to descending date based on the > < symbols
+	// Set to < > to sort ascending
+	return (compA > compB) ? -1 : (compA < compB) ? 1 : 0;
+	});
+	$.each(listitems, function(idx, itm) { mylist.append(itm); });
+   
 
 
+	// SHOW INDIVIDUAL LESSON FUNCTION
 	if(lessonId > 0) {
 		showLesson(lessonId);
 	}
@@ -132,39 +164,16 @@ $(document).ready(function(){
 			var optSubText = "<strong>"+obj[n][0]+" </strong> "+obj[n][1];
 			$('p:last').html(optSubText);
 		}
-
 		$('h1#headerTitle').replaceWith('<h1 class="ui-title" tabindex="0" role="heading" aria-level="1">'+ obj.name[1] + '</h1>');
 		
 		//Create Edit button
 		$("#navbar ul").append('<li class="ui-block-c"><a id="edit" href="additem.html?lessonId='+id+'&op=edit" title="Edit Lesson" class="ui-btn ui-btn-up-c ui-btn-icon-top" rel="external" data-icon="gear" data-corners="false" data-shadow="false" data-iconshadow="true" data-inline="false" data-wrapperels="span" data-iconpos="top"><span class="ui-btn-inner"><span class="ui-btn-text">Edit</span><span class="ui-icon ui-icon-edit ui-icon-shadow"></span></span></a></li>');
 	}
-
-	//Add default data if there is none in local storage
-	function autoFillData(audience){
-		//Store the JSON object in local storage
-		for(var n in audience){
-			var id = Math.floor(Math.random()*10000001);
-			localStorage.setItem(id, JSON.stringify(audience[n]));
-		}
-	} 
-
-	// Sort my lesson list after it has been created
-	var mylist = $('#lesson-list');
-	var listitems = mylist.children('li').get();
-	listitems.sort(function(a, b) {
-	var compA = $(a).text().toUpperCase();
-	var compB = $(b).text().toUpperCase();
-	// Currently set to descending date based on the > < symbols
-	// Set to < > to sort ascending
-	return (compA > compB) ? -1 : (compA < compB) ? 1 : 0;
-	})
-	$.each(listitems, function(idx, itm) { mylist.append(itm); });
-   
    
    
     //Create select field element and populate with options.
+	$('#select select#topics').selectmenu();
 	function makeTopics(){
-
 		$('<select></select>').attr('id', 'topics').attr('data-theme', 'c').appendTo('#select');
 		for (var i=0, j=bibleTopics.length; i<j; i++){
 			var optText = bibleTopics[i];
@@ -174,6 +183,7 @@ $(document).ready(function(){
 		$('#select select#topics').selectmenu('refresh');
 	}
 
+
 	//  Set default date
 	function setDate(){
 		if (!($('#date').val()) ) {
@@ -181,34 +191,15 @@ $(document).ready(function(){
 			var dd = today.getDate();
 			var mm = today.getMonth()+1;//January is 0!
 			var yyyy = today.getFullYear();
-			if(dd<10){dd='0'+dd}
-			if(mm<10){mm='0'+mm}
+			if(dd<10){dd='0'+dd;}
+			if(mm<10){mm='0'+mm;}
 			$('#date').val(mm+'/'+dd+'/'+yyyy);
 		}
 	}
 	
-	function toggleControls(n){
-		switch(n){
-			case "on":
-				$('#lessonForm').css("display", "none");
-				$('displayLink').css("display", "none");
-				$('addNew').css("display", "block");
-				$('required').css("display", "none");
-				$('#pageTitle').html("Submitted Bible Study Lessons");
-				break;
-			case "off":
-				$('lessonForm').css("display", "block");
-				$('clear').css("display", "inline");
-				$('displayLink').css("display", "inline");
-				$('addNew').css("display", "none");
-				$('items').css("display", "none");
-				break;
-			default:
-				return false;
-		}
-	}
 
-	//Save data into local storage.
+
+	// STORE FUNCTION
 	function storeData(key,category){
 		//Create new key if one doesn't exist.
 		if(!key){
@@ -236,6 +227,8 @@ $(document).ready(function(){
 		alert("Bible Study Lesson successfully saved.");
 		window.location.href = "lessons.html?cat="+newItem.audience[1];
 	}
+	
+	
 
 	// EDIT FUNCTION
 	if(op === 'edit') {
@@ -254,6 +247,7 @@ $(document).ready(function(){
 		$('#topics').val(item.topic[1]);
 		$('#focus').val(item.focus[1]);
 		$('#book').val(item.book[1]);
+		
 		var radios = document.forms[0].audience;
 		for(var i=0; i<radios.length; i++){
 			if(radios[i].value == "Adults" && item.audience[1] == "Adults") {
@@ -280,33 +274,33 @@ $(document).ready(function(){
 	}
 	
 	// DELETE FUNCTION
-	function deleteLesson(){
+	function deleteLesson() {
 		var ask = confirm("Are you sure you want to delete this lesson?");
-		if(ask){
+		if (ask) {
 			localStorage.removeItem(lessonId);
-			alert("Lesson was successfully deleted.")
-			if(localStorage.length === 0){
+			alert("Lesson was successfully deleted.");
+			if (localStorage.length === 0) {
 				window.location.href = "index.html";
 			} else {
         		parent.history.back();
 			}
-		}else{
+		} else {
 			alert("Lesson was NOT deleted.");
 		}
 	}
 	
 	// CLEAR ALL FUNCTION
-	function clearLocal(){
-		if(localStorage.length === 0){
+	function clearLocal() {
+		if (localStorage.length === 0) {
 			alert("There is no data to clear.");
-		}else{
+		} else {
 			var ask = confirm("Are you sure you want to delete all lessons?");
-			if(ask){
+			if (ask) {
 				localStorage.clear();
 				alert("All information deleted.");
 				window.location.href = "index.html";
 				return false;
-			}else{
+			} else {
 				alert("Lesson was NOT deleted.");
 			}
 		}
@@ -332,27 +326,27 @@ $(document).ready(function(){
 		//Get Error messages
 		var messageArray = [];
 		//Lesson Name validation
-		if(getLessonName == ""){
+		if (getLessonName === "") {
 			$('#lesson-name').after('<span class="error">Please enter a lesson name.</span>');
 			$('#lesson-name').css("border", "1px solid red") ;
 			hasError = true;
 		}
 		//Author validation
-		if(getAuthor == ""){
-			$('#author').after('<span class="error">Please enter an author name.</span>');
+		if (getAuthor === "") {
+		 	$('#author').after('<span class="error">Please enter an author name.</span>');
 			$('#author').css("border", "1px solid red") ;
 			hasError = true;
 		}
 		//Email validation
 		var re = /^\w+([\.-]?]\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-		if ((getEmail == '') || (!re.test(getEmail))){
+		if ((getEmail === '') || (!re.test(getEmail))){
 			$('#email').after('<span class="error">Please enter a valid email address.</span>');
 			$('#email').css("border", "1px solid red") ;
 			hasError = true;
 		} 
 		
 		//Topic validation
-		if(getTopic === "--Choose A Topic--"){
+		if (getTopic === "--Choose A Topic--") {
 			$('#select > div').after('<span class="error">Please choose a topic.</span>');
 			var topicError = "Please choose a topic.";
 			$('#select > div').css("border", "1px solid red") ;
@@ -360,24 +354,16 @@ $(document).ready(function(){
 		}
 		
 		//Set Errors
-		if(hasError == true) {
+		if (hasError === true) {
 			$('#submit-container').after('<span class="error">Please correct the errors above.</span>');
 			event.preventDefault();
 			return false;
-		}else{
+		} else {
 			//If all is validated, save the data and send the key value from editData
 			storeData(lessonId);
 		}
 	}
 
-	// Get value from URL
-	function getUrlVars() {
-		var vars = {};
-		var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-			vars[key] = value;
-		});
-		return vars;
-	}
 	
 	// Operator Functions
 	$("#delete").click(function() {
@@ -393,5 +379,8 @@ $(document).ready(function(){
 		  validateForm();
 		});
 	}
+	makeTopics();
+	setDate();
+
 
 });
