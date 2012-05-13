3bf038aa6e5d32a7881e1d770a8a1ab6ab345d60
Lucy Hutcheson <hutcheson.lucy@gmail.com>
2012-05-12 23:26:46 -0500

    Added in styling and images.

--- ASD/asdcouch/asdcouch/app/_attachments/js/main.js
+++ ASD/asdcouch/asdcouch/app/_attachments/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$('#home').live('pageinit', function (event) {
 	$.ajax({
 		"url": "_view/categories",
 		"dataType": "json",
@@ -9,7 +9,7 @@ $(document).ready(function() {
 				var description = category.value.description;
 				$('#categoryList').append(
 					$('<li>').append(
-						$('<a>').attr("href", "#")
+						$('<a>').attr("href", "#lessons?cat="+name).attr("data-transition", "slide")
 							.text(name)
 					)
 				);
@@ -20,4 +20,49 @@ $(document).ready(function() {
 			console.log(result);
 		} 
 	});
+
 });
+
+$('#lessons').live('pageinit', function (event) {
+alert('categories');
+	var category = getUrlVars()["cat"];
+	$.ajax({
+		"url": "_view/lessons",
+		"dataType": "json",
+		"success": function(data) {
+			console.log(data);
+			$.each(data.rows, function(index,lesson){
+				var key = lesson.key;
+				var name = lesson.value.name[1];
+				var topic = lesson.value.topic[1];
+				var description = lesson.value.lesson[1];
+				var date = lesson.value.date[1];
+				$('#lessons #lessonList').append(
+					$('<li>').append(
+						$('<a>').attr("href", "#").attr("data-transition", "slide").append(
+								$('<h3>').addClass(key).attr("data-transition", "slide")
+								.text(name),
+							$('<p>').addClass('subhead ' + key).html('<strong>Topic:</strong> '+ topic),
+							$('<p>').addClass('ui-li-desc ' + key).text(description),
+							$('<p>').addClass('date ui-li-aside ' + key).text(date)
+						)
+					)
+				);
+			});
+			$('#lessons #lessonList').listview('refresh');
+		},
+		"error": function(result){
+			console.log(result);
+		} 
+	});
+});
+
+
+// Get value from URL
+function getUrlVars() {
+	var vars = {};
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m,key,value) {
+		vars[key] = value;
+	});
+	return vars;
+}
