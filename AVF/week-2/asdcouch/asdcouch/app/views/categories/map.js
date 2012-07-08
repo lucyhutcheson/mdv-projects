697c7ed2befdc427f2176fe02f4fdeada150417a
Lucy Hutcheson <hutcheson.lucy@gmail.com>
2012-07-07 20:59:32 -0500

    Moving app back to local storage.

--- AVF/week-2/asdcouch/asdcouch/app/views/categories/map.js
+++
@@ -1,8 +0,0 @@
-function(doc) {
-  if (doc._id.substr(0,9) === "category:") {
-	    emit(doc._id.substr(9), {
-	    	"name": doc.name,
-	    	"description": doc.description
-	   });
-  }
-};
--- AVF/week-2/crud/js/main.js
+++ AVF/week-2/crud/js/main.js
@@ -8,122 +8,146 @@ $(document).bind("mobileinit", function(){
 });
     
     
-	// Get value from URL
-	function getUrlVars() {
-		var vars = {};
-		var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m,key,value) {
-			vars[key] = value;
-		});
-		return vars;
-	}
-
-
-	//Variable defaults
-	var discipleId = getUrlVars()["discipleId"];
-
-$('#viewLesson').live('pagehide', function (event) {
-	$('#viewLesson #content').remove();
+    
+/****************************************************************
+ * 
+ * List of Disciples Page                                       *
+ * 
+ ****************************************************************/
+$('#disciples').live('pagehide', function (event) {
+	$('#discipleList').empty();
 });
 
-$('#viewLesson').live('pageshow', function (event) {
-	$('<div></div>').attr('id', 'content').appendTo('#viewLesson').trigger('create');
-	var discipleId = getUrlVars()["discipleId"];
-	var value = localStorage.getItem(discipleId);
+$('#disciples').live('pageshow', function (event) {
+	getLocalData();
+	$('#discipleList').listview();
+	$('#discipleList').listview('refresh');
+});
 
+
+
+
+/****************************************************************
+ * 
+ * View Single Disciple Page                                        *
+ * 
+ ****************************************************************/
+
+$('#viewDisciple').live('pagehide', function (event) {
+	$('#viewDisciple #content .content-container').remove();
+});
+
+$('#viewDisciple').live('pageshow', function (event) {
+	var discipleId = getUrlVars()["id"];
+	var value = localStorage.getItem(discipleId);
 	// convert the string back to an object
 	var obj = JSON.parse(value);
 
-	$('<div></div>').addClass('content-container ui-btn  ui-li ui-corner-top ui-corner-bottom ui-btn-up-c '+discipleId).appendTo('#viewLesson #content');
+	$('<div></div>').addClass('content-container ui-btn  ui-li ui-corner-top ui-corner-bottom ui-btn-up-c '+discipleId).appendTo('#viewDisciple #content');
 
-	if (category === "Adults") {
-		for(var n in obj) {
-			$('<p></p>').addClass('item-details '+discipleId+' '+obj[n][0]).appendTo('div.'+discipleId);
-			var optSubText = "<strong>"+obj[n][0]+" </strong> "+obj[n][1];
-			$('p:last').html(optSubText);
-		}
-	} else if (category === "Men" || category === "Women") {
-		for(var n in obj) {
-			$('<p></p>').addClass('item-details '+discipleId).appendTo('div.'+discipleId);
-			var optSubText = obj[n];
-			$('p:last').html(optSubText);
-		}
+	for(var n in obj) {
+		$('<p></p>').addClass('item-details '+discipleId+' '+obj[n][0]).appendTo('div.'+discipleId);
+		var optSubText = "<strong>"+obj[n][0]+" </strong> "+obj[n][1];
+		$("p.item-details:last").html(optSubText);
 	}
-		
-	//Create Edit button
-	//$("#navbar ul").append('<li id="edit-list" class="ui-block-c"><a id="edit" href="additem.html?discipleId='+discipleId+'&op=edit" title="Edit Lesson" class="ui-btn ui-btn-up-c ui-btn-icon-top" rel="external" data-icon="gear" data-corners="false" data-shadow="false" data-iconshadow="true" data-inline="false" data-wrapperels="span" data-iconpos="top"><span class="ui-btn-inner"><span class="ui-btn-text">Edit</span><span class="ui-icon ui-icon-edit ui-icon-shadow"></span></span></a></li>');
-	
+
+	$('#viewDisciple #edit').attr('href', 'additem.html?id='+discipleId+'&op=edit');
+
 });
 
 
 
-
-
-
-
-$('#addLesson').live('pageinit', function (event) {
-	var op = getUrlVars()["op"];
+/****************************************************************
+ * 
+ * Add New Disciple Page                                        *
+ * 
+ ****************************************************************/
+$('#add-new').live('pageinit', function (event) {
+	$('#discipleid-container').hide();
+});
+$('#add-new').live('pageshow', function (event) {
+    var op = getUrlVars()["op"];
+	var discipleId = getUrlVars()["id"];
 	if(op === 'edit') {
-		editLesson(discipleId);
+		$('input[name=gender').removeAttr('checked');
+		//Change submit button value to edit button
+		$('#pageTitle').text('Edit Disciple');
+		editDisciple(discipleId);
 	}
+	$('#submit').on('click', validateForm);
+
 });
 
+
 	
+/****************************************************************
+ * 
+ * FUNCTIONS		                                            *
+ * 
+ ****************************************************************/
 	
 	// Get Lessons and Build list
-	var getLocalData = function (category) {
+	var getLocalData = function () {
 		$('#errors').empty(); //Reset error messages
 
-		// Create list items from sorted storage array
-		for (var i=0, len=localStorage.length; i<len; i++) {
-			var key = localStorage.key(i);
-			var value = localStorage.getItem(key);
-			// convert the string back to an object
-			var obj = JSON.parse(value);
+		if (localStorage.length > 0) {
+			// Create list items from sorted storage array
+			for (var i=0, len=localStorage.length; i<len; i++) {
+				var key = localStorage.key(i);
+				var value = localStorage.getItem(key);
+				// convert the string back to an object
+				var obj = JSON.parse(value);
 
-	 	 	// $ Create list item
-		 	$('<li></li>').addClass('lesson '+key).attr('data-theme','c').appendTo('#lesson-list');
-			// $ Create anchor
-			$('<a></a>').addClass('anchor '+key).attr('rel','external').attr('href', 'index.html#viewLesson?discipleId='+key+'&cat='+category).attr('data-cache', 'false').attr('data-ajax', false).appendTo('#lesson-list li.'+key);
+		 	 	// $ Create list items
+				$('#disciples #discipleList').append(
+					$('<li>').append(
+						$('<a>').attr("href", "#viewDisciple?id="+key).attr('rel', 'external').attr("data-transition", "slide").append(
+							$('<h4>').addClass(key).attr("data-transition", "slide").text(obj.firstname[1] + ' ' + obj.lastname[1])
+						)
+					)
+				);
+			}
 			
-			// Move date to the bottom if you want to sort based on Header Text
-			if (category === "Adults") {
-				// HEADER TEXT
-				var headerText = obj.firstname[1] + " " + obj.lastname[1];
-				$('<h3></h3>').addClass(key).appendTo('li.'+key+' a.'+key);
-				$('h3.'+key).html(headerText);
+			// Sort my lesson list after it has been created
+			var mylist = $('#discipleList');
+			var listitems = mylist.children('li').get();
+			listitems.sort(function (a, b) {
+				var compA = $(a).text().toUpperCase();
+				var compB = $(b).text().toUpperCase();
+				// Currently set to descending date based on the > < symbols
+				// return (compA > compB) ? -1 : (compA < compB) ? 1 : 0;
+				// Set to < > to sort ascending
+				return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
+			});
+			$.each(listitems, function (idx, itm) { 
+				mylist.append(itm); 
+			});
+	
+			$('#discipleList').listview();
+			$('#discipleList').listview('refresh');
 
-				
-			} 
+		} else {
+			$('#disciples #content').append(
+				$('<div>').append(
+					$('<h4>').text("There are currently no disciples saved.")
+				)
+			);
+			$('#disciples #content').append(
+				$('<div>').html('<a href="additem.html" rel="external" data-role="button" data-theme="a">Add New Disciple</a>')
+			);
+		
 		}
 
-
-		// Sort my lesson list after it has been created
-		var mylist = $('#lesson-list');
-		var listitems = mylist.children('li').get();
-		listitems.sort(function (a, b) {
-			var compA = $(a).text().toUpperCase();
-			var compB = $(b).text().toUpperCase();
-			// Currently set to descending date based on the > < symbols
-			// return (compA > compB) ? -1 : (compA < compB) ? 1 : 0;
-			// Set to < > to sort ascending
-			return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
-		});
-		$.each(listitems, function (idx, itm) { 
-			mylist.append(itm); 
-		});
-
-		$('#lesson-list').listview()
-		$('#lesson-list').listview('refresh');
-
-	}
+	};
 
 
    	
 
 	// EDIT FUNCTION
-	var editLesson = function (discipleId) {
+	var editDisciple = function (discipleId) {
 		$('#pageTitle').html('Edit Info');
 
+		console.log(discipleId);
 		//Grab the data from local storage
 		var value = localStorage.getItem(discipleId);
 		var item = JSON.parse(value);
@@ -137,35 +161,34 @@ $('#addLesson').live('pageinit', function (event) {
 		$('#city').val(item.city[1]);
 		$('#state').val(item.state[1]);
 		$('#zip').val(item.zip[1]);
+		$('select').selectmenu('refresh', true); 
 		
 		$('#birthmonth').val(item.birthmonth[1]);
 		$('#birthday').val(item.birthday[1]);
 		$('#birthyear').val(item.birthyear[1]);
+		$('select').selectmenu('refresh', true); 
 			
 		$('#schoolstatus').val(item.schoolstatus[1]);
-
-		$('input:radio[name=gender]').removeAttr('checked');
-		$('input:radio[name=gender]').checkboxradio('refresh');
-		setRadio(item.gender[1]);
+		$('select').selectmenu('refresh', true); 
+		$('input[value="'+item.gender[1]+'"]').attr('checked', true).checkboxradio('refresh');
 
 		$('#bornagain').val(item.bornagain[1]);
 		$('#frequency').val(item.frequency[1]);
+		$('select').selectmenu('refresh', true); 
 		$('#notes').val(item.notes[1]);
-				
-		//remove initial listener from save button
-
-		//Change submit button value to edit button
-		$('#submit').val('Save Editted Info');
-		$('#submit').button('refresh');
-		$('#addForm').submit(function () {
-			validateForm(discipleId);
-		});
-	}
+		$('#disciple-id').val(discipleId);
+	};
 	
+	
+	
+	// GET RADIO FUNCTION
 	var getRadio = function () {
 		return($('input:radio[name=gender]:checked').val());
-	}
+	};
 	
+	
+	
+	// SET RADIO FUNCTION
 	var setRadio = function (myRadio) {
 		switch(myRadio)
 		{
@@ -178,13 +201,13 @@ $('#addLesson').live('pageinit', function (event) {
 			$('input:radio[name=audience]').checkboxradio('refresh');
 			break;
 		}
-	}
+	};
 	
 
 
 
 	// VALIDATE FUNCTION
-	var validateForm = function (discipleId) {
+	var validateForm = function (e) {
 		var getFirstName = $("#firstname").val();
 		var getLastName = $("#lastname").val();
 		var getEmail = $("#email").val();
@@ -222,54 +245,57 @@ $('#addLesson').live('pageinit', function (event) {
 		
 		//Set Errors
 		if (hasError === true) {
-			$('#submit-container').after('<span class="error">Please correct the errors above.</span>');
-			event.preventDefault();
+			$('#submit-container').before('<br/><span class="error">Please correct the errors above.</span>');
+			$('body,html').animate({scrollTop:0}, 800);
+			e.preventDefault();
 			return false;
+
 		} else {
 			//If all is validated, save the data and send the key value from editData
-			storeData(discipleId);
+			storeData();
 		}
-	}
+	};
+
+
 
 	// STORE FUNCTION
-	$('#submit').on('click', function storeData(key) {
+	function storeData() {
 
-		if(validateForm()) {
-			//Create new key if one doesn't exist.
-			if(!key) {
-				var id = Math.floor(Math.random()*10000001);
-			}else{
-				//Use the existing key.
-				var id = key;
-			}
-			
-			// Gather up all form values and labels.
-			//Find the value of the selected radio button.
-			var newItem = {};
-				newItem.firstname = ["First Name:", $('#firstname').val()];
-				newItem.lastname = ["Last Name:", $('#lastname').val()];
-				newItem.email = ["Email:", $('#email').val()];
-				newItem.phone = ["Phone:", $('#phone').val()];
-				newItem.street = ["Street Address:", $('#street').val()];
-				newItem.city = ["City:", $('#city').val()];
-				newItem.state = ["State:", $('#state').val()];
-				newItem.zip = ["Zip:", $('#zip').val()];
-				newItem.birthmonth = ["Birth Month:", $('#birthmonth').val()];
-				newItem.birthday = ["Birth Day:", $('#birthday').val()];
-				newItem.birthyear = ["Birth Year:", $('#birthyear').val()];
-				newItem.schoolstatus = ["School Status:", $('#schoolstatus').val()];
-				newItem.gender = ["Gender:", getRadio()];
-				newItem.bornagain = ["Born Again Birthday:", $('#bornagain').val()];
-				newItem.frequency = ["Contact Frequency:", $('#frequency').val()];
-				newItem.notes = ["Notes:", $('#notes').val()];
-	
-			//Save data into local storage
-			localStorage.setItem(id, JSON.stringify(newItem));
-			alert("Disciple info successfully saved.");
-			parent.history.back();
+		// Get Lesson Id of existing lesson to edit
+		if( $('#disciple-id').val().length > 0 ) {
+			var id = $('#disciple-id').val();
+			alert(id);
+		} else {
+			var id = Math.floor(Math.random()*10000001);
 		}
+		
+		// Gather up all form values and labels.
+		//Find the value of the selected radio button.
+		var newItem = {};
+			newItem.firstname =  ["First Name:", $('#firstname').val()];
+			newItem.lastname = ["Last Name:", $('#lastname').val()];
+			newItem.email = ["Email: ", $('#email').val()];
+			newItem.phone = ["Phone:", $('#phone').val()];
+			newItem.street = ["Street:", $('#street').val()];
+			newItem.city = ["City:", $('#city').val()];
+			newItem.state = ["State:", $('#state').val()];
+			newItem.zip =  ["Zip:", $('#zip').val()];
+			newItem.birthmonth = ["Birth Month:", $('#birthmonth').val()];
+			newItem.birthday = ["Birth Date:", $('#birthday').val()];
+			newItem.birthyear = ["Birth Year:", $('#birthyear').val()];
+			newItem.schoolstatus = ["School Status:", $('#schoolstatus').val()];
+			newItem.gender = ["Gender:", getRadio()];
+			newItem.bornagain = ["Born Again Date:", $('#bornagain').val()];
+			newItem.frequency = ["Contact Frequency:", $('#frequency').val()];
+			newItem.notes = ["Notes:", $('#notes').val()];
 
-	});
+		//Save data into local storage
+		localStorage.setItem(id, JSON.stringify(newItem));
+		alert("Disciple info successfully saved.");
+		parent.history.back();
+		
+
+	};
 		
 
 	
@@ -297,6 +323,7 @@ $('#addLesson').live('pageinit', function (event) {
 	$("#delete").on('click', function () {
 		var ask = confirm("Are you sure you want to delete this disciple?");
 		if (ask) {
+			var discipleId = getUrlVars()["id"];
 			localStorage.removeItem(discipleId);
 			alert("Disciple was successfully deleted.");
 			if (localStorage.length === 0) {
@@ -310,5 +337,24 @@ $('#addLesson').live('pageinit', function (event) {
 	});
 	
 
+	// Get value from URL
+	function getUrlVars() {
+		var vars = {};
+		var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m,key,value) {
+			vars[key] = value;
+		});
+		return vars;
+	}
+	
+	// Function for confirmation dialog box
+	function areYouSure(text1, text2, button, callback) {
+		$("#sure .sure-1").text(text1);
+		$("#sure .sure-2").text(text2);
+		$("#sure .sure-do").text(button).on("click.sure", function() {
+			callback();
+			$(this).off("click.sure");
+		});
+		$.mobile.changePage("#sure");
+	}
 
 
