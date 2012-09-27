01eff258f8bb1a85959a892011bef6e570c4f90f
Lucy Hutcheson <hutcheson.lucy@gmail.com>
2012-09-27 12:53:29 -0500

    Fixed accessors and mutators in base cookie.

--- AOC2/weekOne/weekOne/BaseRecipe.h
+++ AOC2/weekOne/weekOne/BaseRecipe.h
@@ -19,11 +19,30 @@ typedef enum
 {
     @protected
     int cookieType;
+    int chipNumber;
     NSString *cookieName;
+    float baseWeight;
+    float cookieWeight;
+    float chipWeight;
 }
 
 //-(void)setAttributes:(myCookie)type name:(NSString*)name isDelicious:(BOOL)isDelicious;
 -(id)initWithDetails:(int)type name:(NSString*)name;
+
+
+// Accessors
+-(NSString*)getCookieName;
+-(int)getChipNumber;
+-(float)getbaseWeight;
+-(float)getCookieWeight;
+-(float)getChipWeight;
+
+// Mutators to change the values of the above variables
+-(void)setCookieName:(NSString*)newCookieName;
+-(void)setChipNumber:(int)newChipNumber;
+
+// Calculation Method
+-(float)cookieWeight;
 
 -(void)printName;
 
--- AOC2/weekOne/weekOne/BaseRecipe.m
+++ AOC2/weekOne/weekOne/BaseRecipe.m
@@ -16,14 +16,58 @@
     {
         cookieName = name;
         cookieType = type;
+        baseWeight = 0.43;
+        chipWeight = 0.06;
     }
     return self;
+}
+
+// Accessors
+-(NSString*)getCookieName
+{
+    NSLog(@"I am a %@ cookie.", cookieName);
+    return cookieName;
+}
+-(int)getChipNumber
+{
+    NSLog(@"I have a total number of %d chocolate chips.", cookieType);
+    return cookieType;
+}
+-(float)getbaseWeight
+{
+    NSLog(@"I have a base weight of %f.", baseWeight);
+    return baseWeight;
+}
+-(float)getCookieWeight
+{
+    NSLog(@"I have a weight of %f.", cookieWeight);
+    return cookieWeight;
+}
+-(float)getChipWeight
+{
+    NSLog(@"My chocolate chips weigh %f.", chipWeight);
+    return chipWeight;
+}
+
+// Mutators
+-(void)setCookieName:(NSString*)newCookieName
+{
+    cookieName = newCookieName;
+}
+-(void)setChipNumber:(int)newChipNumber
+{
+    chipNumber = newChipNumber;
+}
+
+-(float)cookieWeight
+{
+    return baseWeight ;
 }
 
 
 -(void)printName
 {
-    NSLog(@"I am eating a %@ cookie with a total of %d chocolate chips in it.", cookieName, cookieType);
+    NSLog(@"I am eating a %@ cookie with a total of %d chocolate chips in it. It weighs %f", cookieName, cookieType, cookieWeight);
 }
 
 @end
